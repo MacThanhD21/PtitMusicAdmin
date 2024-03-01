ca0512e1608f36a1d8187766a13c751d3e49118e
Mac Van Thanh <macthanhkmhd2003@gmail.com>
2024-03-01 19:39:20 +0700

    Update: index.js (PtitMusicAdmin)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const path = require('path');
 const moment = require("moment");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'))
